c3f54a502a50aca06c44656a872ae0e4edef157f
zj <w3313003@163.com>
2018-04-16 23:37:48 +0800

    'send'

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,8 @@ export default class extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            collapsed: false
+            collapsed: false,
+            menuOpenId: ''
         }
     }
     componentWillMount() {
@@ -47,13 +48,23 @@ export default class extends React.Component {
             collapsed: !this.state.collapsed
         })
     }
-    // 递归渲染菜单子项
-    renderMenus(arr) {
+
+    componentWillReceiveProps = nextProps => {
+        this.setState({
+            menuOpenId: nextProps.menus[0].id
+        });
+    }
+    /**
+     * 递归渲染子菜单
+     * @param {any} menus.child 
+     * @returns 
+     */
+    renderSubMenus(arr) {
         return arr.map((sub, index) => {
             if (typeof sub.child !== undefined && sub.child.length > 0) {
                  return (
                     <Menu.SubMenu key={sub.id} title={sub.title}>
-                        {this.renderMenus(sub.child)}
+                        {this.renderSubMenus(sub.child)}
                     </Menu.SubMenu>
                 )
             } else {
@@ -66,27 +77,28 @@ export default class extends React.Component {
         })
     }
     render() {
-        const defaultShow = Array.isArray(this.props.menus) && this.props.menus.length >= 1 ? this.props.menus[0].id : null;
         return (
             <Router>
                 <div>
-                    <div style={{ width: 180 }}>
+                    <div className='leftWrap' style={{ width: 180 }}>
                         <Button type='primary' style={{ marginBottom: 16 }} onClick={this.toggleCollapsed} >
                             切换
                         </Button>
                         <Menu
                             mode="inline"
-                            theme="dark"
                             inlineCollapsed={this.state.collapsed}
                             defaultSelectedKeys={['20']}
-                            defaultOpenKeys={['defaultShow']}
+                            defaultOpenKeys={['sb']}
                         >
-                            {this.props.menus.map((menus, index) => (
-                                <Menu.SubMenu key={menus.id} title={<span>{menus.title}</span>}>
-                                    {this.renderMenus(menus.child)}
+                            {this.props.menus.map((menu, index) => (
+                                <Menu.SubMenu key={index === 0 ? 'sb' : menu.id} title={<span>{menu.title}</span>}>
+                                    {this.renderSubMenus(menu.child)}
                                 </Menu.SubMenu>
                             ))}
                         </Menu>
+                    </div>
+                    <div className='rightWrap'>
+                            123
                     </div>
                 </div>
             </Router>
